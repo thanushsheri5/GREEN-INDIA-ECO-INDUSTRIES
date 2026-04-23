5518c1b25223562bf547c8cb224550ed66bf47fc
thanushsheri5 <thanushsheri5@gmail.com>
2026-04-23 22:41:41 +0530

    Initial upload
    First version of Green India Eco Industries website

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Run app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
