fa66cd18f124120eb7d1177fba02688bf11e6b93
thanushSheri <thanushsheri2@gmail.com>
2026-04-24 10:20:23 +0530

    fixed css issue

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ import os
 
 app = Flask(__name__)
 
-# Home route
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Health check (useful for deployment)
 @app.route('/health')
 def health():
     return "OK", 200
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
